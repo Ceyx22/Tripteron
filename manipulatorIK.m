01e5fc2e54a52d71cef627118444c36fdba52870
Ceyx22 <fm278841@gmail.com>
2023-11-24 21:52:16 -0800

    Base Model URDF Done

--- manipulatorIK.m
+++ manipulatorIK.m
@@ -5,4 +5,7 @@ clear all;
 robot = importrobot('tripteron.urdf');
 % robot = importrobot('sixDOF.urdf');
 axes = show(robot);
+% xlim([-5 5]);
+% ylim([-5 5]);
+% zlim([-5 5]);
 axes.CameraPositionMode = 'auto';

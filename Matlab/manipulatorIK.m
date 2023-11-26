ad1a454c3f0a160858b8d6a76c9afadb439bd46e
Ceyx22 <fm278841@gmail.com>
2023-11-26 08:59:51 -0800

    made urdf xacro files

--- Matlab/manipulatorIK.m
+++ Matlab/manipulatorIK.m
@@ -1,19 +1,7 @@
 clc;
 clear all;
-% tripteron
-% addpath (genpath (strcat (pwd, ' \Dependencies')))
-robot = importrobot(['Project\URDF\tripteronBody.urdf']);
-% robot = importrobot(['Project\URDF\exmple.urdf']);
 
-% robot = importrobot('sixDOF.urdf');
+robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\Description\tripteronBodyFull.xacro']);
+
 axes = show(robot);
-% xlim([-5 5]);
-% ylim([-5 5]);
-% zlim([-5 5]);
 axes.CameraPositionMode = 'auto';
-
-% norm([0.2, -0.2, 0.2])
-
-% rpy="-0.7854 -0.7854 -2.3562"
-
-     

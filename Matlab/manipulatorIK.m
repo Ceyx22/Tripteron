3318875ad6cff3532f9658859c78b7d853bb0da7
Ceyx22 <fm278841@gmail.com>
2023-11-26 05:13:18 -0800

    added ROS and Tripteron body

--- Matlab/manipulatorIK.m
+++ Matlab/manipulatorIK.m
@@ -2,7 +2,9 @@ clc;
 clear all;
 % tripteron
 % addpath (genpath (strcat (pwd, ' \Dependencies')))
-robot = importrobot(['Project\URDF\tripteron.urdf']);
+robot = importrobot(['Project\URDF\tripteronBody.urdf']);
+% robot = importrobot(['Project\URDF\exmple.urdf']);
+
 % robot = importrobot('sixDOF.urdf');
 axes = show(robot);
 % xlim([-5 5]);
@@ -12,3 +14,22 @@ axes.CameraPositionMode = 'auto';
 
 % norm([0.2, -0.2, 0.2])
 
+% rpy="-0.7854 -0.7854 -2.3562"
+
+     <!-- Base_link to Upper arm  -->
+     <joint name="theta1" type="continuous">
+          <parent link="base_link"/>
+          <child  link="upperArm_one"/>
+          <origin xyz="0.0500 -0.0500 -0.0500" rpy="0 0 0"/>  
+          <axis   xyz="0 0 1"/>
+     </joint>
+     
+     <link name="upperArm_one">
+          <visual>
+               <origin xyz="0.0373 -0.0538 -0.0411" rpy="-0.7854 -0.7854 0"/>	 
+               <geometry>
+                    <cylinder radius="0.0127" length="0.1524"/> 
+               </geometry>
+               <material name="cyan"/>
+          </visual>
+     </link>
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # URDF Files 
 **tripteron_base**
 - This has the basic frames for Tripteron
+
 **tripteron**
+
 - This has the body attached to the rails/linear sliders
 **tripteronBody**
+
 - This has the body by itself (still working on this)
+
+**Robot**
+- legs = 6in  in length
+- platform = 6 x 6 x 3

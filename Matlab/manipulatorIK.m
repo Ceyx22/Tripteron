37c7a72cccf244f84be5584fe2af01b6a52c8d10
Ceyx22 <fm278841@gmail.com>
2023-11-26 05:42:27 -0800

    fixed mistake on urdf

--- Matlab/manipulatorIK.m
+++ Matlab/manipulatorIK.m
@@ -16,20 +16,4 @@ axes.CameraPositionMode = 'auto';
 
 % rpy="-0.7854 -0.7854 -2.3562"
 
-     <!-- Base_link to Upper arm  -->
-     <joint name="theta1" type="continuous">
-          <parent link="base_link"/>
-          <child  link="upperArm_one"/>
-          <origin xyz="0.0500 -0.0500 -0.0500" rpy="0 0 0"/>  
-          <axis   xyz="0 0 1"/>
-     </joint>
      
-     <link name="upperArm_one">
-          <visual>
-               <origin xyz="0.0373 -0.0538 -0.0411" rpy="-0.7854 -0.7854 0"/>	 
-               <geometry>
-                    <cylinder radius="0.0127" length="0.1524"/> 
-               </geometry>
-               <material name="cyan"/>
-          </visual>
-     </link>

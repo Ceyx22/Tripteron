6edf509583b4173380b0862388a42e3258329934
Ceyx22 <fm278841@gmail.com>
2023-11-26 14:47:47 -0800

    adjusted ros files

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 from glob import glob
 
-package_name = 'demos'
+package_name = 'Tripteron'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,24 +13,20 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*')),
         ('share/' + package_name + '/rviz',   glob('rviz/*')),
+        ('share/' + package_name + '/Description',   glob('Description/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='robot',
+    maintainer='FM',
     maintainer_email='robot@todo.todo',
-    description='The 133a Project Code Demos',
+    description='Tripteron Demo',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'boolean_publisher = demos.boolean_publisher:main',
-            'float_publisher   = demos.float_publisher:main',
-            'point_publisher   = demos.point_publisher:main',
-            'balldemo          = demos.balldemo:main',
-            'fencedemo         = demos.fencedemo:main',
-            'interactivedemo   = demos.interactivedemo:main',
-            'pirouette         = demos.pirouette:main',
-            'pirouetteandwave  = demos.pirouetteandwave:main',
+            'boolean_publisher = Util.boolean_publisher:main',
+            'float_publisher   = Util.float_publisher:main',
+            'point_publisher   = Util.point_publisher:main',
         ],
     },
 )

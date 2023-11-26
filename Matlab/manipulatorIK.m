08b36fb94f6f475dd7fd7bfbccdcc663820070fe
Ceyx22 <fm278841@gmail.com>
2023-11-26 11:27:12 -0800

    adjusted resting position

--- Matlab/manipulatorIK.m
+++ Matlab/manipulatorIK.m
@@ -2,6 +2,7 @@ clc;
 clear all;
 
 robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\Description\tripteronBodyFull.xacro']);
+% robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\Description\tripteronBody.xacro']);
 
 axes = show(robot);
 axes.CameraPositionMode = 'auto';

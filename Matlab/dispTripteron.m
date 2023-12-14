660b2250ae5cdd297876062a7c3870dfe635d3bb
Ceyx22 <fm278841@gmail.com>
2023-12-13 19:21:40 -0800

    new urdf

--- Matlab/dispTripteron.m
+++ Matlab/dispTripteron.m
@@ -1,7 +1,7 @@
 clc;
 clear all;
 
-robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\Description\tripteronBodyFull.xacro']);
+robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\robotDescription\tripteron.urdf.xacro']);
 % robot = importrobot(['D:\HW\Fall Smore\ME_133a\Project\Description\tripteronBody.xacro']);
 
 axes = show(robot);
